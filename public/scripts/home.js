8545195cfb76ea1bdae18cd69ae120205e591fe1
G-Linares <gerrylinares@gmail.com>
2021-11-05 14:42:35 -0600

    Ya quedo terminado la nav bar responsiva con animaciones de JS

---
+++ public/scripts/home.js
@@ -0,0 +1,32 @@
+/* Se agrega funcionalidad a la nav bar desde aqui*/
+
+const navDeslizar = () => {
+    const burger = document.querySelector('.burger');
+    const nav = document.querySelector('.nav-links');
+    const navLinks = document.querySelectorAll('.nav-links li');
+
+    // activar barra nav
+    burger.addEventListener('click', () => {
+        nav.classList.toggle('nav-activada');
+
+        //animar los links, se va a ir metiendo de uno en una dependiendo su index
+        navLinks.forEach((link, index) => {
+            //se hace este if para verificar si esta abierto el menu y cerrarlo
+            if(link.style.animation){
+                link.style.animation = ''
+            }else{
+                link.style.animation = `navLinkDesaparece 0.5s ease forwards ${index/7 + 0.6}s`
+            }
+        });
+
+        burger.classList.toggle("https://www.google.com/favicon.ico");
+
+
+    });
+
+    
+    
+    
+}
+
+navDeslizar();
