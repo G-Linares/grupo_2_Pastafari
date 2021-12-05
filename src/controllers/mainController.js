9588a2fe191ca8be378990f8d2f20dbe57862562
G-Linares <gerrylinares@gmail.com>
2021-12-05 12:52:19 -0600

    Agrego modularidad al menu, hago una pequeña base de datos

--- src/controllers/mainController.js
+++ src/controllers/mainController.js
@@ -1,12 +1,13 @@
 const platillos = require('../data/platillos');
 const reviews = require('../data/reviews');
+const menu = require('../data/menuCompleto');
 
 const mainController = {
     home: (req,res) => {
         res.render('home', {platillos, reviews});
     },
     menu: (req,res) => {
-        res.render('menu')
+        res.render('menu', {menu});
     },
     carrito: (req,res) => {
         res.render('carrito',{platillos});
