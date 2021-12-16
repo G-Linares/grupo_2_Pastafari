7baeb16977fa7d15168318c6f5598fded74929be
G-Linares <gerrylinares@gmail.com>
2021-12-15 18:39:39 -0600

    agrego el error 404

--- app.js
+++ app.js
@@ -27,10 +27,11 @@ app.set('views', [path.join(__dirname + '/src/views/users'), path.join(__dirname
 app.use('/', mainRouters);
 app.use('/producto', productRouters);
 
-// ************ catch 404 and forward to error handler ************
-app.use((req, res, next) => next(createError(404)));
+
+
 
 // ************ error handler ************
+app.use((req, res, next) => next(createError(404)));
 app.use((err, req, res, next) => {
   // set locals, only providing error in development
   res.locals.message = err.message;
