b7dd5fe9f13b0a5f52d48a91f4ec690ec47fbed7
MariusMerk <oficialmariom@gmail.com>
2021-12-17 20:56:29 -0600

    Se agrega una validación suave a la imagen al subir un producto

--- src/routers/mainRouter.js
+++ src/routers/mainRouter.js
@@ -1,47 +1,60 @@
-const express = require('express');
+const express = require("express");
 
-const mainController = require('../controllers/mainController')
+const mainController = require("../controllers/mainController");
 
-const multer = require('multer');
+const whitelist = ['image/png', 'image/jpeg', 'image/jpg'];
 
-let storage = multer.diskStorage({ 
-    destination: function (req, file, cb) {
-    cb(null, 'public/img/products');
-}, 
-filename: function (req, file, cb) {
-    cb(null, `img_${file.originalname}`)
-}
+const multer = require("multer");
+let storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "public/img/products");
+  },
+  filename: function (req, file, cb) {
+    cb(null, `img_${file.originalname}`);
+  },
 });
+let upload = multer({ storage: storage, fileFilter: (req, file, cb) => {
+    if (!whitelist.includes(file.mimetype)) {
+      console.log("---Ejecutó el if porque no es un archivo aceptado----");
+      cb(null, false);
+      return cb(new Error("Tipo de archivo invalido"));
+    }
+    console.log("---Es un archivo aceptado y continua con las operaciones----");
+    cb(null, true);
+  }, });
 
-let upload = multer({storage:storage})
 
 const mainRouters = express.Router();
 
 // render a home
-mainRouters.get('/', mainController.home);
+mainRouters.get("/", mainController.home);
 
 //search a algun platillo con el nombre exacto
-mainRouters.get('/search',mainController.search);
+mainRouters.get("/search", mainController.search);
 
 //render a todo el menu
-mainRouters.get('/menu', mainController.menu);
+mainRouters.get("/menu", mainController.menu);
 
 //render al carrito
-mainRouters.get('/carrito', mainController.carrito);
+mainRouters.get("/carrito", mainController.carrito);
 
 //render a login
-mainRouters.get('/createAccount', mainController.login);
-mainRouters.post('/createAccount',upload.single(),mainController.loginNew);
+mainRouters.get("/createAccount", mainController.login);
+mainRouters.post("/createAccount", upload.single(), mainController.loginNew);
 
 //render a todo el menu que se puede editar
-mainRouters.get('/editar', mainController.index);
-mainRouters.post('/editar', upload.single('image'),mainController.agregar);
+mainRouters.get("/editar", mainController.index);
+mainRouters.post(
+  "/editar",
+  /*evaluar,**/ upload.single("image"),
+  mainController.agregar
+);
 
 //se elimina un producto del JSON
-mainRouters.delete('/delete/:id', mainController.borrar); 
+mainRouters.delete("/delete/:id", mainController.borrar);
 
 //render a un articulo en especifico
-mainRouters.get('/editarProducto/:id', mainController.editando);
-mainRouters.put('/editarProducto/:id', mainController.update);
+mainRouters.get("/editarProducto/:id", mainController.editando);
+mainRouters.put("/editarProducto/:id", mainController.update);
 
 module.exports = mainRouters;
