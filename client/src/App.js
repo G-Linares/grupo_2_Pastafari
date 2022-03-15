5ff79051267efa1570199f8b699229089bad9c8d
G-Linares <gerrylinares@gmail.com>
2022-03-15 10:49:48 -0600

    Creo una version nueva de pastafari con todo lo que tenia pero en React

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <>
+        <div className="Wrapper">
+          <h1> Pastafari </h1>  
+        </div> 
+    </>
+  );
+};
+
+export default App;
+
