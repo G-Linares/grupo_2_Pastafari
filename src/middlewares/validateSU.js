290ae5e816ef51f2bc77c334b307f59845dda6eb
Mario Mercado <oficialmariom@gmail.com>
2022-03-25 13:16:07 -0600

    Validación crear usuario full-1, requiere manejar tamaño de archivo (5mb actual, más crashea)

--- src/middlewares/validateSU.js
+++ src/middlewares/validateSU.js
@@ -5,32 +5,64 @@ const validateSU = [
     .trim()
     .notEmpty()
     .withMessage("Nombre no puede estar vacio")
+    .bail()
     .isLength({ min: 3 })
     .withMessage("Nombre debe tener al menos 3 caractéres"),
   body("lastname")
     .trim()
     .notEmpty()
     .withMessage("Apellido no puede estar vacio")
+    .bail()
     .isLength({ min: 3 })
     .withMessage("Apellido debe tener al menos 3 caractéres"),
   body("username")
     .trim()
     .notEmpty()
     .withMessage("Usuario no puede estar vacio")
+    .bail()
     .isLength({ min: 3 })
     .withMessage("Usuario debe tener al menos 3 caractéres"),
   body("email")
     .trim()
     .notEmpty()
     .withMessage("Email no puede estar vacio")
+    .bail()
     .isEmail()
     .withMessage("Inserta un email valido"),
   body("password")
     .trim()
     .notEmpty()
     .withMessage("Password no puede estar vacio")
+    .bail()
     .isLength({ min: 6 })
     .withMessage("Password debe tener al menos 6 caractéres"),
+  body("coPassword")
+  .trim()
+  .notEmpty()
+  .withMessage("Confirmación de password no puede estar vacia")
+  .bail()
+  .isLength({min: 6})
+  .withMessage("La confirmación del password debe tener 6 caractéres")
+  .bail()
+  .custom( async (coPassword, {req}) => {
+    const password = req.body.password;
+    if (password !== coPassword) {
+      throw new Error("Los password no son iguales")
+    }
+  }),
+  body("img")
+  .custom((value, { req }) => {
+    switch (req.file.mimetype) {
+      case "image/png":
+        return true;
+      case "image/jpg":
+        return true;
+      case "image/jpeg":
+        return true;
+      default:
+        throw new Error("La imagen debe ser png, jpg o jpeg");
+    }
+  }),
 ];
 
 module.exports = validateSU;
